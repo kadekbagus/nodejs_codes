e2a82dae4d16f178096989a29aef91fb676ae27e
kadekbagus <kadek@dominopos.com>
2017-10-19 16:40:38 +0800

    modify codes

--- project1/app.js
+++ project1/app.js
@@ -2,23 +2,31 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('kadek'));
+// console.log(_.isString(true));
+// console.log(_.isString('kadek'));
 
-//console.log(process.argv);
+// console.log(process.argv);
 
+const argv = yargs.argv;
 var command = process.argv[2];
-console.log('Command:', command);
+console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+// var command = process.argv[2];
+// console.log('Command:', command);
 
 if (command === 'add') {
-	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-	console.log('Listing all notes');
+	notes.getAll();
 } else if(command === 'read') {
-	console.log('Reading note');
+	notes.readNote(argv.title);
 } else if(command === 'remove') {
-	console.log('Removing note');
+	notes.removeNote(argv.title);
 } else {
 	console.log('Command not recognized');
 }
