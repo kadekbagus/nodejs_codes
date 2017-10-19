81462bcc0da959411a91aedd7735ccd5c394eb0c
kadekbagus <kadek@dominopos.com>
2017-10-19 16:05:06 +0800

    add project1 folder

---
+++ project1/app.js
@@ -0,0 +1,24 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+console.log(_.isString(true));
+console.log(_.isString('kadek'));
+
+//console.log(process.argv);
+
+var command = process.argv[2];
+console.log('Command:', command);
+
+if (command === 'add') {
+	console.log('Adding new note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else if(command === 'read') {
+	console.log('Reading note');
+} else if(command === 'remove') {
+	console.log('Removing note');
+} else {
+	console.log('Command not recognized');
+}
